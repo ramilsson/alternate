c9983a0e6846fb150f6149ccfc9f113e078b2ca1
ramilsson <khuzh.n@gmail.com>
2024-05-02 02:12:11 +0300

    enable cors using @fastify/cors plugin

--- src/build-server.ts
+++ src/build-server.ts
@@ -1,4 +1,5 @@
 import Fastify, { type FastifyServerOptions } from 'fastify';
+import cors from '@fastify/cors';
 import database from './database';
 import { project } from './project';
 import { collection } from './collection';
@@ -7,7 +8,13 @@ import { resource } from './resource';
 export const buildServer = (options?: FastifyServerOptions) => {
   const fastifyServer = Fastify(options);
 
+  fastifyServer.register(cors, {
+    origin: true,
+    credentials: true,
+  });
+
   fastifyServer.register(database);
+
   fastifyServer.register(project);
   fastifyServer.register(collection);
   fastifyServer.register(resource);
