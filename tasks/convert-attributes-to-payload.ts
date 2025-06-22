dddc782e04382bb663768e6f3c45def1fc95165f
ramilsson <khuzh.n@gmail.com>
2025-06-23 00:13:09 +0300

    add task to convert resource attributes to payload

---
+++ tasks/convert-attributes-to-payload.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env -S npx tsx
+
+/**
+ * @file This file contains an one-off task that converts resource `attributes` to `payload`.
+ */
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const resources = await prisma.resource.findMany({
+    include: { attributes: true },
+  });
+
+  await prisma.$transaction(
+    resources.map((resource) => {
+      const payload = resource.attributes.reduce<Record<string, unknown>>(
+        (acc, attribute) => {
+          const value = (() => {
+            if (!attribute.value) {
+              return null;
+            } else if (attribute.type === 'LITERAL_NUMBER') {
+              return Number(attribute.value);
+            } else if (attribute.type === 'LITERAL_BOOLEAN') {
+              return Boolean(Number(attribute.value));
+            } else if (attribute.type === 'LITERAL_JSON') {
+              return JSON.parse(attribute.value);
+            } else {
+              return String(attribute.value);
+            }
+          })();
+
+          acc[attribute.name] = value;
+          return acc;
+        },
+        {}
+      );
+
+      return prisma.resource.update({
+        where: { id: resource.id },
+        data: { payload: payload },
+      });
+    })
+  );
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
